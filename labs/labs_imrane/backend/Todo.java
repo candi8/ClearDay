28c56539b79df6ee4c70d782aa1c162a33d4918e
imrane belkoufa <imranebelkoufa@gmail.com>
2025-08-28 11:26:31 +0100

    finally the backend part but still not finalised

---
+++ labs/labs_imrane/backend/Todo.java
@@ -0,0 +1,20 @@
+
+
+public class Todo {
+    private int id;
+    private String task;
+    private boolean completed;
+
+    public Todo(int id, String task) {
+        this.id = id;
+        this.task = task;
+        this.completed = false;
+    }
+
+    public int getId() { return id; }
+    public String getTask() { return task; }
+    public boolean isCompleted() { return completed; }
+
+    public void setTask(String task) { this.task = task; }
+    public void setCompleted(boolean completed) { this.completed = completed; }
+}
